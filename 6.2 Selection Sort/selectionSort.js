c0b3cc7bd6234e53b482a559d2b882980ac89d3d
batman8056 <manimanish8056@gmail.com>
2025-03-19 23:07:21 +0530

    update

---
+++ 6.2 Selection Sort/selectionSort.js
@@ -0,0 +1,20 @@
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // Swap elements
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    return arr;
+};
+
+// Example
+console.log(selectionSort([5, 2, 9, 1,-3,  5, 6]));
