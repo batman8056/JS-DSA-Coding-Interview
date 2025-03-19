0ab5cc549dded23dc913cd32321b537776b0f1f9
batman8056 <manimanish8056@gmail.com>
2025-03-19 19:32:39 +0530

    update

---
+++ 6.0 Search Algorithms/binarySearch.js
@@ -0,0 +1,14 @@
+function search(arr,val){
+    let min = 0
+    let max =  arr.length -1;
+    let middle = Math.floor((min + max) /2);
+
+        while(arr[middle] !== val){
+            console.log(min, max, middle);
+            if(val < arr[middle]) max = middle -1;
+            else min = middle +1;
+            middle = Math.floor((min + max) /2);
+         }
+        return arr[middle] == val ? middle : -1;
+}
+console.log(search([1, 2, 3, 4, 5, 6],5));
